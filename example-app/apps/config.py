bf49b09906ac4304aea947df90c94cbc12d0f661
Marcin Żeleźny <35434151+cineqqq@users.noreply.github.com>
2023-12-07 21:45:42 +0100

    Database info update

--- example-app/apps/config.py
+++ example-app/apps/config.py
@@ -31,11 +31,11 @@ class ProductionConfig(Config):
     # PostgreSQL database
     SQLALCHEMY_DATABASE_URI = (
         f"{os.getenv('DB_ENGINE', 'postgresql')}://"
-        f"{os.getenv('DB_USERNAME', 'appseed_db_usr')}:"
-        f"{os.getenv('DB_PASS', 'pass')}@"
-        f"{os.getenv('DB_HOST', 'localhost')}:"
-        f"{os.getenv('DB_PORT', '3306')}/"
-        f"{os.getenv('DB_NAME', 'appseed_db')}"
+        f"{os.getenv('DB_USERNAME', 'mzelezny_baza_user')}:"
+        f"{os.getenv('DB_PASS', 'rf00O9pjhuHSkjbeAcD2YbayHIEBOZ1r')}@"
+        f"{os.getenv('DB_HOST', 'dpg-clogbf946foc73c6g4k0-a')}:"
+        f"{os.getenv('DB_PORT', '5432')}/"
+        f"{os.getenv('DB_NAME', 'mzelezny_baza')}"
     )
 
 
